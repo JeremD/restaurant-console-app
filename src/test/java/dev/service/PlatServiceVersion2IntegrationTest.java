471292664b1a73f46a75ef3613ada7aea5448fa5
JeremYD <delpech.jeremy@hotmail.fr>
2020-07-15 16:37:09 +0200

    Spring Test: TP 3.3

---
+++ src/test/java/dev/service/PlatServiceVersion2IntegrationTest.java
@@ -0,0 +1,40 @@
+package dev.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import dev.dao.PlatDaoMemoire;
+import dev.entite.Plat;
+import dev.exception.PlatException;
+
+@SpringJUnitConfig(classes = { PlatServiceVersion2.class, PlatDaoMemoire.class, Plat.class })
+@ActiveProfiles( {"memoire", "serviceV2" })
+public class PlatServiceVersion2IntegrationTest {
+
+	@Autowired
+	PlatServiceVersion2 service;
+	
+	@Test
+	void ajouterPlatValide() {
+		
+		Plat ajoutPlat = new Plat("LasagneThon", 2400);
+		service.ajouterPlat(ajoutPlat.getNom(), ajoutPlat.getPrixEnCentimesEuros());
+
+		List<Plat> resultat = service.listerPlats();
+		assertThat(resultat).extracting(Plat::getNom).contains(ajoutPlat.getNom());
+	}
+
+	@Test
+	void ajouterPlatprixInvalide() {
+		assertThatThrownBy(() -> service.ajouterPlat("pizzaJambon", 400)).isInstanceOf(PlatException.class)
+		.hasMessage("le prix d'un plat doit être supérieur à 10 €");
+	}
+
+}
